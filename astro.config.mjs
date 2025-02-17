e5fe88953d7ea4b708e70e05212e3fb7c1bf416d
Dakoro <david.korobetski@gmail.com>
2025-02-17 05:53:16 +0100

    chore: fix typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
     integrations: [icon()],
-    site: 'https://dakoro.github.io'
+    site: 'https://dakoro.github.io',
 });
