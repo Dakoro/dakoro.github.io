52abb0066fb67b921e3cc2981193bbc64d6bcaa1
Dakoro <david.korobetski@gmail.com>
2025-02-18 02:52:06 +0100

    feat: reinit project

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-    ],
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
