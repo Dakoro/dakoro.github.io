92450510f47d30622d2395ffa41c9bed245cfcbb
Dakoro <david.korobetski@gmail.com>
2025-07-14 17:20:39 +0200

    feat: add an audio feature for blog article

--- website/src/content.config.ts
+++ website/src/content.config.ts
@@ -12,6 +12,7 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		audioSummary: z.string().optional(),
 		lang: z.enum(['en', 'fr']).optional(),
 	}),
 });
